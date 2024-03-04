cb13a8ffcfbc21ad63a55cd66770dad796df32ad
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2024-03-03 22:11:06 -0300

    Fix broken nullable attributes logic in receipt product mapping

--- AugustaGourmet.Api.Domain/Entities/InventoryHandling/InventoryParameter.cs
+++ AugustaGourmet.Api.Domain/Entities/InventoryHandling/InventoryParameter.cs
@@ -9,32 +9,24 @@ public class InventoryParameter : BaseEntity
 {
     [Column("CODIGO_EMPRESAId")]
     public int CompanyId { get; set; }
-    [ForeignKey("CompanyId")]
-    public Company Company { get; set; } = null!;
 
     [Column("CODIGO_FORNECEDORId")]
     public int SupplierId { get; set; }
-    [ForeignKey("SupplierId")]
-    public Supplier Supplier { get; set; } = null!;
 
     [Column("CODIGO_PRODUTOId")]
     public int InventoryProductId { get; set; }
-    [ForeignKey("InventoryProductId")]
-    public Product InventoryProduct { get; set; } = null!;
 
     [Column("ESTOQUE_MINIMO")]
     public decimal MinStockQuantity { get; set; }
+
     [Column("UNIDADE_EST_MINIMO_Id")]
     public int MinStockUnitId { get; set; }
-    [ForeignKey("MinStockUnitId")]
-    public UnitMeasure MinStockUnit { get; set; } = null!;
 
     [Column("ESTOQUE_MAXIMO")]
     public decimal MaxStockQuantity { get; set; }
+
     [Column("UNIDADE_EST_MAXIMO_Id")]
     public int MaxStockUnitId { get; set; }
-    [ForeignKey("MaxStockUnitId")]
-    public UnitMeasure MaxStockUnit { get; set; } = null!;
 
     [Column("CONTAGEM_OBRIGATORIA")]  //TODO: change to bool
     public int CountIsMandatory { get; set; }
@@ -44,4 +36,19 @@ public class InventoryParameter : BaseEntity
 
     [NotMapped]
     public decimal? Quantity { get; set; }
+
+    [ForeignKey("InventoryProductId")]
+    public Product InventoryProduct { get; set; } = null!;
+
+    [ForeignKey("SupplierId")]
+    public Supplier Supplier { get; set; } = null!;
+
+    [ForeignKey("CompanyId")]
+    public Company Company { get; set; } = null!;
+
+    [ForeignKey("MinStockUnitId")]
+    public UnitMeasure MinStockUnit { get; set; } = null!;
+
+    [ForeignKey("MaxStockUnitId")]
+    public UnitMeasure MaxStockUnit { get; set; } = null!;
 }
