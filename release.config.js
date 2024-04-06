4333c571bce6b4cdc1d337e45b1e724ed941d7e7
Luiz Eduardo Bartolassi <luizekbartolassi@gmail.com>
2024-04-06 15:54:03 -0300

    ci: edit semantic-versioning config file structure

--- release.config.js
+++ release.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-    brancher: 'main',
-    verifyConditions: [
-        '@semantic-release/changelog',
-        '@semantic-release/git',
-        '@semantic-release/github',
-    ],
-    publish: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/changelog',
-        '@semantic-release/git',
-    ],
+    branches: 'development',  // TODO: Change to 'main' when ready to release
+    plugins: [
+        '@semantic-release/commit-analyzer', 
+        '@semantic-release/release-notes-generator', 
+        '@semantic-release/github'
+    ]
 }
